a20d38c553a63f1cdf39840d6e5ab9d5a42f8434
Empero6 <empero6@github.com>
2021-04-11 20:08:02 -0400

    Finished the code

--- src/main/java/LineByLine.java
+++ src/main/java/LineByLine.java
@@ -5,7 +5,15 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        while (!value.isEmpty()) {
+            String[] check = value.split(" ");
+            for (String s : check) {
+                System.out.println(s);
+            }
 
+            value = scanner.nextLine();
+        }
 
     }
 }
